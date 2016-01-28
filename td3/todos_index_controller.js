77fb91d1260a7fe5042ec6612ed46a2570cbb21e
schleft <kevin.legougne@gmail.com>
2016-01-28 17:31:08 +0100

    Modification todo

--- td3/todos_index_controller.js
+++ td3/todos_index_controller.js
@@ -2,16 +2,23 @@ angular.module('todos').controller('TodosIndexController', ['$scope', '$routePar
 	$scope.list = List.get({id: $routeParams['id']})
 	$scope.todos = Todo.query({list_id: $routeParams['id']})	
 
+  $scope.updateTodo = function(val){
+    angular.forEach($scope.todos, function(value, key) {
+      if(value._id.$oid == $routeParams['id']){  
+        todolists.lists[key].label = val;
+      }
+    });
+    Todo.update({list_id:$routeParams['id'], id:$routeParams['id_todo']}, {todo: {text: val}});
+    console.log("OK");
+  }
 
 	$scope.updateList = function(val){
-    	console.log(val);
-      	angular.forEach(todolists.lists, function(value, key) {
-        	if(value._id.$oid == $routeParams['id']){     
-          		console.log(todolists.lists[key]);
-          		todolists.lists[key].label = val;
-        	}
-      	});
-        List.update({id:$routeParams['id']},{list:$scope.list});
+    angular.forEach(todolists.lists, function(value, key) {
+      if(value._id.$oid == $routeParams['id']){  
+        todolists.lists[key].label = val;
+      }
+    });
+    List.update({id:$routeParams['id']},{list:$scope.list});
   };
 
   $scope.createTodo = function(){
@@ -22,15 +29,15 @@ angular.module('todos').controller('TodosIndexController', ['$scope', '$routePar
   }
 
   $scope.deleteTodo = function(){
-      t = new Todo($scope.Todo);
-      t.$delete({list_id:$routeParams['id'], id:$routeParams['id_todo']});
-      console.log($scope.todos);
+    t = new Todo($scope.Todo);
+    t.$delete({list_id:$routeParams['id'], id:$routeParams['id_todo']});
+    console.log($scope.todos);
 
-      angular.forEach($scope.todos, function(value, key) {
-        if(value._id.$oid == $routeParams['id_todo']){
-          $scope.todos.splice(key,1);
-        }
-      });
+    angular.forEach($scope.todos, function(value, key) {
+      if(value._id.$oid == $routeParams['id_todo']){
+        $scope.todos.splice(key,1);
+      }
+    });
   }
 
   $scope.deleteList= function(){
